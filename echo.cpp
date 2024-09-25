ea8d4f5abe8418997eda2438a4511a6827f8ce11
kritikajoshi815 <128927037+kritikajoshi815@users.noreply.github.com>
2024-09-25 10:57:14 +0530

    Add files via upload

---
+++ echo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "header.hpp"
+using namespace std;
+
+void print(string s)
+{
+    // cout << "output is" << endl;
+    cout << s;
+    cout << endl;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+main:
+	g++ -o a.out main.cpp echo.cpp pwd.cpp  cd.cpp ls.cpp pinfo.cpp search.cpp io.cpp  header.hpp
